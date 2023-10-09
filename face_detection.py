1d5d4aa9c36e918f08e6255aedc83a0f033c205d
orberman1 <orberman5@gmail.com>
2023-10-09 14:36:58 +0300

    with square padding

--- face_detection.py
+++ face_detection.py
@@ -1,11 +1,16 @@
 from face_detector import YoloDetector
-import numpy as np
-from PIL import Image, ImageDraw
 from argparser import parse_args
 from utils import image_utils
 
 
 def detect_faces_in_images(model, img, bs):
+    """
+    detect the faces in the image
+    @param model: the detector
+    @param img: the images to detect faces on
+    @param bs: batch size for inference
+    @return: bounding boxes for each frame in format: [[(x00, y00, x01, y01), ..., (x0n, y0n, x1n, y1n)], ... [(...)]]
+    """
     if bs != -1:
         bboxes = []
         list_chunked = [img[i:i + bs] for i in range(0, len(img), bs)]
@@ -19,6 +24,13 @@ def detect_faces_in_images(model, img, bs):
 
 
 def load_face_detector(target_size=720, device="cpu", min_face=20):
+    """
+    return face detector model
+    @param target_size: the size of the squared image
+    @param device: device to run on
+    @param min_face: minimum possible size of faces in images
+    @return: a model
+    """
     return YoloDetector(target_size=target_size, device=device, min_face=min_face)
 
 
@@ -26,7 +38,7 @@ def detection_pipeline(video):
     """
     preprocess the video and returns the processes video and bboxes
     @param video: a list of frames
-    @return: process video and a list of lists of bboxes, one for each frame
+    @return: process video and a list of lists of bboxes, one for each frame. If visualize, return also video with bboxes
     """
     args = parse_args()
     frame = video[0]
