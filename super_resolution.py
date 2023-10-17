635575db2d9c8f0b0c6792aaac37aa5a488027cf
Your Name <you@example.com>
2023-10-17 16:02:27 +0300

    sharp edges

--- super_resolution.py
+++ super_resolution.py
@@ -48,6 +48,7 @@ def upscale_image(img, model, device="cpu"):
     @param model: super resolution model that should return a new image
     @return: a new upsampled image
     """
+    img = sharp_edges(img)
     img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
     img = img.astype(np.float32) / 255
     imgt = img2tensor(img)
