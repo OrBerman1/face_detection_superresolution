489e238659aec84fe060a6fbc5d4eddb523b4ee6
Your Name <you@example.com>
2023-10-19 14:02:55 +0300

    data running

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from image_utils import read_video, read_image, read_images_from_dir
 from detection_and_super_resolution import video_face_detection_and_super_resolution
 
 args = parse_args()
-if args.video_path is None and args.image_path is None and args.folder_path:
+if args.video_path is None and args.image_path is None and args.folder_path is None:
     raise IOError("no input path was given, please give image_path, video_path, or folder_path")
 if (args.video_path and args.image_path) or (args.video_path and args.folder_path) or (args.image_path and args.folder):
     raise IOError("2 of the 3 possible path options: video_path, image_path, and folder_path, most be None")
